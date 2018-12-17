4d864f606af97aec3f30f2e9f54a50807548148b
Adrián Villanueva <adrianvillanueva997@gmail.com>
2018-12-17 16:29:35 +0100

    NLP Testing time!
    I think it is almost done, but i want to try it a little bit more

--- proyecto_1/Text_Procesing.py
+++ proyecto_1/Text_Procesing.py
@@ -1,44 +1,80 @@
 import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
-
+from nltk.probability import FreqDist
 from proyecto_1 import File_Manager
+import string
 
 
 class Text_Processing:
+    """
+    All NLP process is done here
+    """
+
     def __init__(self, reviews):
+        """
+        Class constructor
+        :param reviews:
+        """
         self.reviews = reviews
-        self.tokens = []
-        self.filtered_reviews = []
-        self.stop_words = set(stopwords.words('spanish'))
 
-    def to_lower(self):
-        self.reviews = [word.lower() for word in self.reviews]
-
-    def tokenizer(self):
-        for review in self.reviews:
+    def __tokenizer(self, reviews):
+        """
+        Tokenization process, recieves a list of reviews and returns a list with a list of tokens for each review
+        :param reviews:
+        :return: list
+        """
+        tokens = []
+        for review in reviews:
             token = word_tokenize(text=review)
-            self.tokens.append(token)
+            tokens.append(token)
+        return tokens
 
-    def filter_stop_words(self):
-        for token in self.tokens:
-            self.filtered_reviews = [word for word in token if word not in self.stop_words]
-
-    def stemming(self):
-        sno = nltk.stem.SnowballStemmer('spanish')
-        for token in self.tokens:
+    def __filter_stop_words(self, tokens):
+        """
+        Removal of stop words and punctuation
+        :param tokens:
+        :return:
+        """
+        filtered_reviews = []
+        stop_words = set(stopwords.words('spanish'))
+        for token in tokens:
             for word in token:
-                if word not in self.stop_words:
-                    self.filtered_reviews.append(sno.stem(word))
+                if word not in stop_words and word not in string.punctuation:
+                    filtered_reviews.append(word)
+        return filtered_reviews
 
+    def __stemmer(self, filtered_reviews):
+        """
+        Stemmer, must receive a list of filtered reviews, otherwise it won't work as expected
+        :param filtered_reviews:
+        :return:
+        """
+        stemmer = nltk.SnowballStemmer('spanish')
+        stemmed_reviews = []
+        for word in filtered_reviews:
+            stemmed_reviews.append(stemmer.stem(word))
+        return stemmed_reviews
 
-if __name__ == '__main__':
-    fm = File_Manager.File_Manager(path=r'/home/xiao/Documents/proyecto_Comp1/dataset_entrenamiento/buenas/')
-    reviews = fm.extract_data_from_files()
-    tp = Text_Processing(reviews)
-    tp.to_lower()
-    tp.tokenizer()
-    print(tp.tokens)
-    tp.filter_stop_words()
-    tp.stemming()
-    print(tp.filtered_reviews)
+    def process_reviews(self):
+        """
+        Public function that does all the NLP process
+        :return:
+        """
+        lower_reviews = [word.lower() for word in self.reviews]
+        tokens = self.__tokenizer(lower_reviews)
+        filtered_reviews = self.__filter_stop_words(tokens)
+        stemmed_reviews = self.__stemmer(filtered_reviews)
+
+        return stemmed_reviews
+
+    def graph_reviews(self):
+        """
+        Dani didn't want this graph, but it may be useful (or not), returns a frequency word distribution graph
+        :return:
+        """
+        processed_reviews = self.process_reviews()
+        fdist = FreqDist(processed_reviews)
+        print(fdist)
+        fdist.plot(30, cumulative=False)
+        return fdist
