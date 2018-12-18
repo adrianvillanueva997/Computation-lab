9fb29cad5645286cbdd0a0e1632f8681053b7a60
Adrián Villanueva <adrianvillanueva997@gmail.com>
2018-12-18 12:09:07 +0100

    preparing stuff WIP

--- proyecto_1/test.py
+++ proyecto_1/test.py
@@ -1,4 +1,5 @@
 from proyecto_1 import File_Manager, Text_Procesing
+from sklearn.feature_extraction.text import CountVectorizer
 
 if __name__ == '__main__':
     fm = File_Manager.File_Manager(path='/home/xiao/Documents/proyecto_Comp1/dataset_entrenamiento/buenas/')
@@ -6,3 +7,6 @@ if __name__ == '__main__':
     tp = Text_Procesing.Text_Processing(reviews)
     processed_reviews = tp.process_reviews()
     print(processed_reviews)
+    cv = CountVectorizer(tokenizer=None)
+    cv.fit(processed_reviews)
+    print(cv.vocabulary_)
