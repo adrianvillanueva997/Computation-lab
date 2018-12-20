78d699a1c1de4b9e117af57ed6f1d2df31163bbb
Adrián Villanueva <adrianvillanueva997@gmail.com>
2018-12-20 14:07:39 +0100

    Creating a pandas DF
    In order to make the training easier, i am working with pandas to make a dataframe with the text labeled and those things

--- proyecto_1/test.py
+++ proyecto_1/test.py
@@ -2,24 +2,37 @@ from proyecto_1 import File_Manager, Text_Procesing
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.feature_extraction.text import HashingVectorizer
+from sklearn.model_selection import train_test_split
+import pandas as pd
 
 if __name__ == '__main__':
     # /home/xiao/datasets/proyecto_computacion/dataset_entrenamiento/buenas/
     # /home/xiao/Documents/proyecto_Comp1/dataset_entrenamiento/buenas/
-    fm = File_Manager.File_Manager(path='/home/xiao/datasets/proyecto_computacion/dataset_entrenamiento/buenas/')
+    fm = File_Manager.File_Manager(path='/home/xiao/Documents/proyecto_Comp1/dataset_entrenamiento/buenas/')
     reviews = fm.extract_data_from_files()
     tp = Text_Procesing.Text_Processing(reviews)
     processed_reviews = tp.process_reviews()
     print(processed_reviews)
-    cv = CountVectorizer(tokenizer=None)
+
+    names = ['text', 'label']
+    df = pd.DataFrame(columns=names)
+
+    df = df.append(pd.DataFrame(processed_reviews, columns=['text']), sort=False)
+    for col in df.columns():
+        df.loc[df[col] == 'label', col] = 0
+    print(df)
+
+"""
+cv = CountVectorizer(tokenizer=None, stop_words=None)
     cv.fit(processed_reviews)
     print(cv.vocabulary_)
     encode = cv.transform(processed_reviews)
     print(encode)
     print(type(encode))
-    print(encode.toarray())
-
-    tv = TfidfVectorizer()
+    data = encode.toarray()
+    X_train, X_test, y_train, y_test = train_test_split(data, 1, test_size=0.1, random_state=69)
+    print(X_test)
+tv = TfidfVectorizer()
     tv.fit(processed_reviews)
     vector = tv.transform(processed_reviews)
     print(vector.shape)
@@ -30,3 +43,4 @@ if __name__ == '__main__':
     print(content.shape)
     print(content.toarray())
 
+"""
