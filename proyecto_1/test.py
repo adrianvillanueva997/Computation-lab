9f736f226b983bbe6885b765634eb176fc94d42c
Adrián Villanueva <adrianvillanueva997@gmail.com>
2018-12-20 22:46:56 +0100

    small sklearn test

--- proyecto_1/test.py
+++ proyecto_1/test.py
@@ -4,6 +4,9 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.feature_extraction.text import HashingVectorizer
 from sklearn.model_selection import train_test_split
 import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neighbors import KNeighborsClassifier
 
 if __name__ == '__main__':
     # /home/xiao/datasets/proyecto_computacion/dataset_entrenamiento/buenas/
@@ -42,5 +45,24 @@ tv = TfidfVectorizer()
     content = vec.transform(processed_reviews)
     print(content.shape)
     print(content.toarray())
+    
+
+
+X = ['the food was really delicious', 'the food was really terrible']
+
+y = [5, 2]
+x_data = CountVectorizer().fit_transform(X)
+gnb = GaussianNB()
+a = gnb.fit(x_data.toarray(), y)
+print(a)
+
+Z = ['the food was awesome', 'the food was pretty bad']
+z_data = CountVectorizer().fit_transform(Z)
+gnb.fit(z_data.toarray(), y)
+b = gnb.predict(z_data.toarray())
+print(b)
+c = gnb.predict_proba(z_data.toarray())
+print(c)
+
 
 """
