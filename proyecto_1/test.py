f7dfceeacd8178afda55e0fe60fceca6da64c9af
Adrián Villanueva <adrianvillanueva997@gmail.com>
2018-12-28 21:24:52 +0100

    Models + Vectorizers done!

--- proyecto_1/test.py
+++ proyecto_1/test.py
@@ -1,11 +1,4 @@
-from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
-from sklearn.model_selection import train_test_split, learning_curve, ShuffleSplit
-from sklearn.naive_bayes import MultinomialNB
-import numpy as np
-from proyecto_1 import File_Manager, Text_Procesing
-import pandas as pd
-from sklearn.metrics import confusion_matrix
-from scikit_testing import plotter
+from proyecto_1 import File_Manager, Text_Procesing, Vectorizer, Models
 
 if __name__ == '__main__':
     # /home/xiao/datasets/proyecto_computacion/dataset_entrenamiento/buenas/
@@ -28,49 +21,15 @@ if __name__ == '__main__':
     n_processed_reviews = tp.process_reviews()
     print(n_processed_reviews)
 
-    data = {
-        'reviews': [],
-        'label': [],
-    }
-    for review in g_processed_reviews:
-        data['reviews'].append(review)
-        data['label'].append(1)
+    vectorizer = Vectorizer.Vectorizer()
+    df = vectorizer.generate_dataframe(g_processed_reviews, b_processed_reviews, n_processed_reviews)
+    vectorized_reviews = vectorizer.count_vectorizer(df)
+    X_train, X_test, y_train, y_test = vectorizer.generate_train_test_data(vectorized_reviews)
+    models = Models.Models(X_train, y_train, X_test, y_test)
+    probability, conf_matrix = models.multinomial_naive_bayes()
 
-    for review in b_processed_reviews:
-        data['reviews'].append(review)
-        data['label'].append(2)
+    print(probability)
+    print(conf_matrix)
 
-    for review in n_processed_reviews:
-        data['reviews'].append(review)
-        data['label'].append(3)
-
-    df = pd.DataFrame(data)
-
-    print(df)
-    print(df.shape)
-    count_vectorizer = CountVectorizer()
-    vectorized_reviews = count_vectorizer.fit_transform(data['reviews'])
-    transformer = TfidfTransformer().fit(vectorized_reviews)
-    counts = transformer.transform(vectorized_reviews)
-    X_train, X_test, y_train, y_test = train_test_split(vectorized_reviews, data['label'], test_size=0.1,
-                                                        random_state=10)
-
-    print(X_train)
-    print(X_test)
-    print(y_train)
-    print(y_test)
-    model = MultinomialNB(alpha=0.01)
-    model.fit(X_train, y_train)
-    print(model)
-
-    predicted = model.predict(X_test)
-
-    print(np.mean(predicted == y_test))
-    print(confusion_matrix(y_test, predicted))
-
-    cv = ShuffleSplit(n_splits=100, test_size=0.2, random_state=0)
-    title = 'learning curve'
-    curve = learning_curve(model, X_train, y_train)
-
-    a = plotter.plot_learning_curve(model, 'Learning curve', X_test, y_test, cv=cv, n_jobs=4, ylim=(0, 1))
-    a.show()
+    plot = models.generate_plot_learning_curve(title="Learning Curve", X=X_test, y=y_test)
+    plot.show()
