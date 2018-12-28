dd1218a4f1c097184fdbf69fe6544fa614bfac89
Daniel Vilar <danvgdesign@gmail.com>
2018-12-28 17:41:38 +0100

    Implemented Main Screen

--- KivyTesting/main_screen.py
+++ KivyTesting/main_screen.py
@@ -10,15 +10,23 @@ view = Builder.load_file("main_screen_view.kv")
 
 class MainScreen(Screen):
 
-    def handle_event(self, instance, code, **kwargs):
-        pass
+    def handle_event(self, instance, key, **kwargs):
+        if key == "TRAIN":
+            self.goto_train()
+        elif key == "CLASSIFY":
+            self.goto_classify()
+        elif key == "HELP":
+            self.goto_help()
 
     def goto_train(self):
+        print("TODO: Implement goto_train")
         pass
 
     def goto_classify(selfs):
+        print("TODO: Implement goto_classify")
         pass
 
     def goto_help(self):
+        print("TODO: Implement goto_help")
         pass
 
